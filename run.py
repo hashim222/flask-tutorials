5c778f83dfc298d9879741b536e2de8429bffe9d
hashim222 <aslamhashim19@gmail.com>
2022-07-07 18:22:07 +0000

    updated json file and for loop were added about html page

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", company=data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact")
